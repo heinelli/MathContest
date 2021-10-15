73f91cb55d86f465f85877b91c8c69fe9b82d5ff
Elliot Heiner <elliotheiner1997@gmail.com>
2021-10-14 18:10:39 -0600

    Add random number generation functions.

--- MathContest/MathContestForm.vb
+++ MathContest/MathContestForm.vb
@@ -8,8 +8,16 @@ Option Strict On
 Option Explicit On
 Public Class MathContestForm
     Private Sub MathContestForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim num1 As Integer = RandomNumber()
+        Dim num2 As Integer = RandomNumber()
+        Dim correctAnswer As Integer = num1 + num2
+
         AddRadioButton.Checked = True
+        FirstNumberTextBox.Text = CStr(num1)
+        SecondNumberTextBox.Text = CStr(num2)
     End Sub
+
+
 
     Private Sub ExitCommand(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
@@ -20,6 +28,7 @@ Public Class MathContestForm
             If CInt(StudentAgeTextBox.Text) < 7 Or CInt(StudentAgeTextBox.Text) > 11 Then
                 MsgBox("Student not eligible to compete.")
                 StudentAgeTextBox.Text = ""
+                StudentAgeTextBox.Focus()
             End If
         Catch ex As Exception
             StudentAgeTextBox.Text = ""
@@ -32,9 +41,38 @@ Public Class MathContestForm
             If CInt(StudentGradeTextBox.Text) < 1 Or CInt(StudentGradeTextBox.Text) > 4 Then
                 MsgBox("Student not eligible to compete.")
                 StudentGradeTextBox.Text = ""
+                StudentGradeTextBox.Focus()
             End If
         Catch ex As Exception
             StudentGradeTextBox.Text = ""
+            StudentGradeTextBox.Focus()
         End Try
+    End Sub
+
+    Function RandomNumber() As Integer
+        Dim rndNumber As Integer
+        Randomize()
+        rndNumber = CInt(System.Math.Floor(Rnd() * (20 + 1)))
+        Return rndNumber
+    End Function
+
+    Function RandomOperator() As Integer
+        Dim rndOperator As Integer
+        Randomize()
+        rndOperator = CInt(System.Math.Floor(Rnd() * (3 + 1)))
+        Return rndOperator
+    End Function
+
+    Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
+        Dim operationType As Integer = RandomOperator()
+        If operationType = 0 Then
+            AddRadioButton.Checked = True
+        ElseIf operationType = 1 Then
+            SubtractRadioButton.Checked = True
+        ElseIf operationType = 2 Then
+            MultiplyRadioButton.Checked = True
+        Else
+            DivideRadioButton.Checked = True
+        End If
     End Sub
 End Class
